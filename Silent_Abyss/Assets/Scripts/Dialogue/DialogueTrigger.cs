d1236b9ef01a83148bc7a329b2501f15778521f2
Yashwa122 <yyousaf2021@elam.co.uk>
2022-05-12 12:43:23 +0100

    script

--- Silent_Abyss/Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Silent_Abyss/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -7,5 +7,42 @@ public class DialogueTrigger : MonoBehaviour
     [Header("Visual Cue")]
     [SerializeField] private GameObject visualCue;
 
+    [Header("Ink JSON")]
+    [SerializeField] private GameObject inkJSON;
 
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            visualCue.SetActive(true);
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Collider.gameObject.tag == "Fighter")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (Collider.gameObject.tag == "Fighter")
+        {
+            playerInRange = false;
+        }
+    }
 }
